b783ba2ae9a712dddd8d9b96960860cf208f9b42
Shashwat Doshi <doshishashwat17@gmail.com>
2020-11-08 18:44:13 +0530

    Deployment v1.0

---
+++ precache-manifest.b5173b5d217f61920d578898b5ede43c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20efe5c2f9011697dbab956a1e0aa826",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb0c7eb317a65aab9848",
+    "url": "/static/css/main.022eb82c.chunk.css"
+  },
+  {
+    "revision": "f08f5c2faeba83ba75d8",
+    "url": "/static/js/2.3b189aff.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.3b189aff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb0c7eb317a65aab9848",
+    "url": "/static/js/main.d4687f71.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "2cad743faacd4a37d9c9582448eadc68",
+    "url": "/static/media/resume.2cad743f.pdf"
+  },
+  {
+    "revision": "d824bd86be6cca283bc99f47cee0674a",
+    "url": "/static/media/shashwatdoshi.d824bd86.png"
+  }
+]);
